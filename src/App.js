3875fc97f30f4be7d3739bb3d520d2ebeea8ccef
Luna Shuqair <luna.shuqair89@gmail.com>
2021-04-12 14:47:58 -0400

    first push

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Background from "./components/background";
+
+function App() {
+  return <Background />;
+}
+
+export default App;
+
---
+++ src/components/background.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Background() {
+  return (
+    <div>
+      <h1>Welcome to my Protfolio</h1>
+    </div>
+  );
+}
+
+export default Background;
