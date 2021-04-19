b53e6dcdf1c1108dc4cbd104b40aa7ad814881c8
Luna Shuqair <luna.shuqair89@gmail.com>
2021-04-19 15:26:59 -0400

    update

--- src/pages/Me.js
+++ src/pages/Me.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import image from "../images/image.gif";
 
 
 function Me() {
   return (
     <div>
-      <Hero backgroundImage="https://i.pinimg.com/originals/ef/c6/d9/efc6d99cf4ac33217e661068775de927.gif">
+      <Hero backgroundImage={image}>
         <h1>WELCOME TO MY Portforlio</h1>
         
       </Hero>
